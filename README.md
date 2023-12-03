1e503f5035c317eb7208115a2f91b9a2700f3be6
211502024 <211502024@smail.nju.edu.cn>
2023-12-03 12:32:33 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NJU-FLA-Project
 NJU FLA (Fall 2023) — Project
+
+南京大学 形式语言与自动机 2023 
+
+课程主页：[Formal Languages and Automata (fla23course.github.io)](https://fla23course.github.io/)
---
+++ framework/turing-project/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+    
+    if(argc == 1){
+        return 1;
+    }
+    cout<<"This is for testing"<<endl;
+    return 0;
+}
