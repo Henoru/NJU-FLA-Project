efb80135f972665142ec9ac0aabb86c4faa8a79c
211502024 <211502024@smail.nju.edu.cn>
2024-01-03 00:44:08 +0800

    make submit

---
+++ submit.sh
@@ -0,0 +1,7 @@
+mkdir 211502024-贺卓宇
+cp -r programs/ 211502024-贺卓宇/
+cp -r turing-project/ 211502024-贺卓宇/
+cp CMakeLists.txt 211502024-贺卓宇/
+cp README.md 211502024-贺卓宇/
+zip -r 211502024-贺卓宇.zip 211502024-贺卓宇
+rm -r 211502024-贺卓宇
